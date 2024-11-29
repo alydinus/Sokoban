84b8835b19b2b58f1024bc203a5ef72cae74ed3b
eaitkaziev <aitkazieverdan@gmail.com>
2024-11-29 13:02:35 +0100

    reseted dev to mvc pattern

--- Levels.java
+++ Levels.java
@@ -3,77 +3,111 @@ import java.io.IOException;
 
 public class Levels {
     private int level;
+
     public Levels() {
         level = 1;
     }
-    public String nextLevel() {
-        String currentLevel = null;
+    public int[][] nextLevel() {
+        int[][] array = null;
         switch (level) {
             case 1:
-                currentLevel = getFirstLevel();
+                array = getFirstLevel();
                 break;
             case 2:
-                currentLevel = getSecondLevel();
+                array = getSecondLevel();
                 break;
             case 3:
-                currentLevel = getThirdLevel();
+                array = getThirdLevel();
                 break;
             case 4:
-                currentLevel = getLevelFromFile("levels/level4.sok");
+                array = getFourthLevel();
                 break;
             case 5:
-                currentLevel = getLevelFromFile("levels/level5.sok");
-
+                array = getLevelFromFile("levels/level5.sok");
+                break;
+            case 6:
+                array = getLevelFromFile("levels/level6.sok");
+                break;
+            case 7:
+                array = getLevelFromFile("levels/level7.sok");
+                break;
             default:
-
+                level = 1;
+                array = getFirstLevel();
         }
-        return currentLevel;
-    }
-    private String getFirstLevel() {
-        return  "1111111\n" +
-                "1000001\n" +
-                "1203001\n" +
-                "1040001\n" +
-                "1000001\n"+
-                "1111111\n";
+        level = level + 1;
+        return array;
     }
 
-    private String getSecondLevel() {
-        return  "11111\n"+
-                "10021\n"+
-                "10301\n"+
-                "14001\n"+
-                "11111\n";
-    }
-    private String getThirdLevel() {
-        return  "11111111111\n" +
-                "14000000041\n" +
-                "10003000001\n" +
-                "10000000001\n" +
-                "10030000001\n" +
-                "10011110031\n" +
-                "12014000001\n" +
-                "10010000001\n" +
-                "11111111111\n";
+    private int[][] getFirstLevel() {
+        return new int[][]{
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                {2, 0, 0, 1, 0, 0, 0, 0, 0, 2},
+                {2, 0, 0, 0, 0, 3, 0, 3, 0, 2},
+                {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                {2, 0, 4, 0, 0, 0, 3, 0, 0, 2},
+                {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                {2, 0, 0, 0, 4, 0, 0, 0, 0, 2},
+                {2, 0, 0, 0, 0, 0, 0, 0, 4, 2},
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+        };
     }
 
-    private String getLevelFromFile(String fileName) {
+    private int[][] getSecondLevel() {
+        return new int[][]{
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                {2, 0, 0, 1, 0, 0, 0, 0, 0, 2},
+                {2, 0, 0, 3, 0, 0, 0, 0, 3, 2},
+                {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                {2, 0, 0, 0, 0, 0, 3, 0, 0, 2},
+                {2, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+                {2, 0, 0, 0, 2, 2, 0, 0, 0, 2},
+                {2, 0, 0, 4, 2, 4, 0, 0, 4, 2},
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+        };
+    }
+
+    private int[][] getThirdLevel() {
+        return new int[][]{
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {2, 0, 2, 4, 0, 0, 2, 4, 0, 2},
+                {2, 0, 0, 2, 0, 3, 2, 0, 0, 2},
+                {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                {2, 0, 0, 1, 0, 0, 0, 0, 0, 2},
+                {2, 0, 2, 0, 0, 0, 3, 0, 0, 2},
+                {2, 0, 3, 0, 0, 2, 0, 0, 0, 2},
+                {2, 0, 0, 0, 2, 0, 0, 0, 0, 2},
+                {2, 0, 0, 0, 2, 4, 0, 0, 0, 2},
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+        };
+    }
+
+    private int[][] getFourthLevel() {
+        return new int[][]{
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {2, 0, 0, 0, 4, 0, 4, 0, 0, 2},
+                {2, 0, 4, 0, 0, 0, 0, 0, 0, 2},
+                {2, 0, 3, 0, 0, 3, 0, 3, 0, 2},
+                {2, 1, 0, 2, 2, 2, 2, 2, 2, 2},
+                {2, 2, 2, 2,}
+        };
+    }
+
+    private int[][] getLevelFromFile(String fileName) {
         FileInputStream inputStream = null;
-        StringBuilder container;
+        StringBuilder container = null;
         try {
             inputStream = new FileInputStream(fileName);
             container = new StringBuilder();
             int unicode;
             while ((unicode = inputStream.read()) != -1) {
                 char symbol = (char) unicode;
-                if (('0' <= symbol && symbol <= '6') || symbol == '\n') {
+                if (('0' <= symbol && symbol <= '4') || symbol == '\n') {
                     container.append(symbol);
                 }
             }
-            if (container.length() > 0 && container.charAt(container.length() - 1) != '\n') {
-                container.append('\n');
-            }
-
         } catch (IOException ioe) {
             System.out.println("ioe" + ioe);
             container = null;
@@ -86,6 +120,57 @@ public class Levels {
                 System.out.println("ioe" + ioe);
             }
         }
-        return container.toString();
+        int[][] array = null;
+        if (container != null) {
+            array = convertToArray(container);
+        }
+        return array;
+    }
+
+    private int[][] convertToArray(StringBuilder container) {
+        int count = 0;
+        for (int i = 0; i < container.length(); i++) {
+            char element = container.charAt(i);
+            if (element == '\n') {
+                count = count + 1;
+            }
+            if (i == container.length() - 1) {
+                count = count + 1;
+            }
+        }
+        int[][] array = new int[count][];
+        int index = 0;
+        int indexArray = 0;
+        for (int i = 0; i < container.length(); i++) {
+            char element = container.charAt(i);
+            if (element == '\n') {
+                array[indexArray] = new int[index];
+                indexArray = indexArray + 1;
+                index = 0;
+                continue;
+            }
+            index = index + 1;
+            if (i == container.length() - 1) {
+                array[indexArray] = new int[index];
+            }
+        }
+        int row = 0;
+        int column = 0;
+        for (int z = 0; z < container.length(); z++) {
+            char element = container.charAt(z);
+            if (element == '\n') {
+                row = row + 1;
+                column = 0;
+                continue;
+            }
+            int number = Integer.parseInt("" + element);
+            array[row][column] = number;
+            column = column + 1;
+
+        }
+        return array;
+    }
+    public int getLevelNumber(){
+        return level;
     }
 }
--- Model.java
+++ Model.java
@@ -1,241 +1,56 @@
-
-
 public class Model {
-
-    private Node startNode;
-    private Node newLine;
-    private Node[] goals;
     private Viewer viewer;
-    private Node player;
-    private Graph desktop;
-    private boolean stateDesktop;
-    private Levels levels;
+    private int[][] desktop;
 
-    public Model(Viewer viewer) {
+    public Model(Viewer viewer){
         this.viewer = viewer;
-        levels = new Levels();
         initialization();
     }
 
     private void initialization() {
-        String level = levels.nextLevel();
-
-        int playerNumber = 0;
-        int boxesNumber = 0;
-        int goalsNumber = 0;
-        char element;
-
-        for (int i = 0; i < level.length(); i++) {
-            element = level.charAt(i);
-            if (element == '2') {
-                playerNumber = playerNumber + 1;
-            } else if (element == '3') {
-                boxesNumber = boxesNumber + 1;
-            } else if (element == '4') {
-                goalsNumber = goalsNumber + 1;
-            }
-        }
-
-        if ((playerNumber != 1) || (boxesNumber == 0) || (goalsNumber == 0) || (boxesNumber != goalsNumber)) {
-            stateDesktop = false;
-            return;
-        }
-
-        desktop = new Graph(level);
-        if (desktop.getStartNode() == null || desktop == null) {
-            stateDesktop = false;
-            System.err.println("Error: startNode is null. Graph was not built correctly.");
-            return;
-        }
-
-        startNode = desktop.getStartNode();
-        System.out.println("Start node: " + startNode.getType());
-        player = getPlayer();
-        goals = getGoals(goalsNumber);
-        stateDesktop = true;
     }
-
-    private Node[] getGoals(int goalsNumber) {
-        Node[] goals = new Node[goalsNumber];
-        Node currentNode = startNode;
-        newLine = startNode.getDown();
-        int index = 0;
-        while (true) {
-            if (currentNode.getType().equals("Goal")) {
-                goals[index] = currentNode;
-                index = index + 1;
-            }
-            if ((currentNode.getRight() == null && currentNode.getDown() == null) || goals[goalsNumber - 1] != null) {
+    public void move(String direction){
+        switch (direction){
+            case "Left":
+                moveLeft();
                 break;
-            }
-            if (currentNode.getRight() == null) {
-                currentNode = newLine;
-                newLine = newLine.getDown();
-                continue;
-            }
-            currentNode = currentNode.getRight();
-        }
-        return goals;
-    }
-
-    public boolean getState() {
-        return stateDesktop;
-    }
-
-    public Node getPlayer() {
-        System.out.println("Graph structure:");
-        Node row = startNode;
-
-        // Печать графа для отладки
-        while (row != null) {
-            Node col = row;
-            while (col != null) {
-                System.out.print(col.getType() + " ");
-                col = col.getRight();
-            }
-            System.out.println();
-            row = row.getDown();
-        }
-
-        Node currentNode = startNode;
-        newLine = startNode.getDown();
-
-        // Поиск игрока
-        while (currentNode != null) {
-            if ("Player".equals(currentNode.getType())) {
-                return currentNode;
-            }
-
-            if (currentNode.getRight() == null) {
-                if (newLine == null) {
-                    throw new IllegalStateException("Error: newLine is null. Graph is incomplete.");
-                }
-                currentNode = newLine;
-                newLine = newLine.getDown();
-            } else {
-                currentNode = currentNode.getRight();
-            }
-        }
-
-        throw new IllegalStateException("Player node not found in the graph.");
-    }
-
-    public void move(String direction) {
-        if (direction.equals("Up")) {
-            moveUp();
-        } else if (direction.equals("Down")) {
-            moveDown();
-        } else if (direction.equals("Left")) {
-            moveLeft();
-        } else if (direction.equals("Right")) {
-            moveRight();
-        } else {
-            return;
+            case "Up":
+                moveUp();
+                break;
+            case "Right":
+                moveRight();
+                break;
+            case "Down":
+                moveDown();
+                break;
+            default:
+                break;
         }
         viewer.update();
+        checkGoal();
+        win();
     }
 
-    public Graph getDesktop() {
-        return desktop;
+    private void win() {
     }
 
-    private void moveUp() {
-        Node upNode = player.getUp();
-        if (upNode == null || upNode.getType().equals("Wall")) {
-            return;
-        }
-        if (upNode.getType().equals("Box") || upNode.getType().equals("BoxOnGoal")) {
-            Node upBoxNode = upNode.getUp();
-            if (upBoxNode == null || upBoxNode.getType().equals("Wall") ||
-                    upBoxNode.getType().equals("Box") || upBoxNode.getType().equals("BoxOnGoal")) {
-                return;
-            }
-            processBoxMove(upNode, upBoxNode);
-        }
-
-        processPlayerMove(player, upNode);
-        player = upNode;
+    private void checkGoal() {
+        
     }
 
     private void moveDown() {
-        Node downNode = player.getDown();
-        if (downNode == null || downNode.getType().equals("Wall")) {
-            return;
-        }
-        if (downNode.getType().equals("Box") || downNode.getType().equals("BoxOnGoal")) {
-            Node downBoxNode = downNode.getDown();
-            if (downBoxNode == null || downBoxNode.getType().equals("Wall") ||
-                    downBoxNode.getType().equals("Box") || downBoxNode.getType().equals("BoxOnGoal")) {
-                return;
-            }
-            processBoxMove(downNode, downBoxNode);
-        }
-
-        processPlayerMove(player, downNode);
-        player = downNode;
-    }
-
-    private void moveLeft() {
-        Node leftNode = player.getLeft();
-        if (leftNode == null || leftNode.getType().equals("Wall")) {
-            return;
-        }
-        if (leftNode.getType().equals("Box") || leftNode.getType().equals("BoxOnGoal")) {
-            Node leftBoxNode = leftNode.getLeft();
-            if (leftBoxNode == null || leftBoxNode.getType().equals("Wall") ||
-                    leftBoxNode.getType().equals("Box") || leftBoxNode.getType().equals("BoxOnGoal")) {
-                return;
-            }
-            processBoxMove(leftNode, leftBoxNode);
-        }
-
-        processPlayerMove(player, leftNode);
-        player = leftNode;
     }
 
     private void moveRight() {
-        Node rightNode = player.getRight();
-        if (rightNode == null || rightNode.getType().equals("Wall")) {
-            return;
-        }
-        if (rightNode.getType().equals("Box") || rightNode.getType().equals("BoxOnGoal")) {
-            Node rightBoxNode = rightNode.getRight();
-            if (rightBoxNode == null || rightBoxNode.getType().equals("Wall") ||
-                    rightBoxNode.getType().equals("Box") || rightBoxNode.getType().equals("BoxOnGoal")) {
-                return;
-            }
-            processBoxMove(rightNode, rightBoxNode);
-        }
-
-        processPlayerMove(player, rightNode);
-        player = rightNode;
     }
 
-    private void processBoxMove(Node boxNode, Node targetNode) {
-        if (boxNode.getType().equals("BoxOnGoal")) {
-            boxNode.setType("Goal");
-        } else {
-            boxNode.setType("Floor");
-        }
-
-        if (targetNode.getType().equals("Goal")) {
-            targetNode.setType("BoxOnGoal");
-        } else {
-            targetNode.setType("Box");
-        }
+    private void moveLeft() {
     }
 
-    private void processPlayerMove(Node playerNode, Node targetNode) {
-        if (playerNode.getType().equals("PlayerOnGoal")) {
-            playerNode.setType("Goal");
-        } else {
-            playerNode.setType("Floor");
-        }
+    private void moveUp() {
+    }
 
-        if (targetNode.getType().equals("Goal")) {
-            targetNode.setType("PlayerOnGoal");
-        } else {
-            targetNode.setType("Player");
-        }
+    public boolean getState() {
+        return false;
     }
 }
